afe244df87315b12625f1c5169d99d82ed2dcb2b
isunenUnizar <isunen@unizar.es>
2025-06-25 15:25:29 +0200

    Update main_script.R

--- R/main_script.R
+++ R/main_script.R
@@ -23,3 +23,31 @@ zaragoza <- esp_get_prov(prov = "Zaragoza")
 # Mostrar el polígono
 print(zaragoza)
 
+library(sf)
+library(purrr)
+library(furrr)
+library(dplyr)  # Para manipulación de datos
+
+procesar_archivo <- function(archivo, poligono) {
+  # Leer solo datos dentro del polígono usando wkt_filter
+  wkt <- st_as_text(st_geometry(poligono))
+  
+  datos <- sf::read_sf(archivo, query = NULL, wkt_filter = wkt)
+  
+  # Calcular medias de las variables
+  medias <- datos %>%
+    summarise(
+      MeanTemperature = mean(MeanTemperature, na.rm = TRUE),
+      Precipitation = mean(Precipitation, na.rm = TRUE),
+      RelativeHumidity = mean(RelativeHumidity, na.rm = TRUE)
+    )
+  
+  return(medias)
+}
+plan(multisession, workers = 2)  
+
+archivos <- list.files(path = "raw_data", pattern = "\\.gpkg$", full.names = TRUE)
+resultados <- future_map_dfr(archivos, ~ procesar_archivo(.x, zaragoza))
+print(resultados)
+
+
